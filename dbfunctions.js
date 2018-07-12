08ba6e6c7c14acb7e0b015d0356a48257b80b8bf
Jay "Danger" Farnsworth <DrDanger@Jays-MacBook-Pro.local>
2018-07-12 14:23:49 -0600

    changed start script

--- dbfunctions.js
+++ dbfunctions.js
@@ -1,7 +1,7 @@
 const monk = require('monk')
 
 // Connection URL
-const url = 'localhost/inventory';
+const url = 'lmongodb://test:test11@ds235461.mlab.com:35461/heroku_s2sn9c54';
 
 const db = monk(url);
 
